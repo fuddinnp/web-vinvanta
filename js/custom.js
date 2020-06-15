26ded78e9fb57c1fa7f175f3fcd48c3fe923ad96
Fahad Uddin <fd1024@gmail.com>
2020-06-15 18:11:12 +0500

    new build with animations

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,9 @@
-$(window).on('scroll', function() {
-    var scrollTop = $(window).scrollTop();
 
-    if (scrollTop >= 200 && scrollTop <= 400) {
-        $('.discover-path-img').stop().animate({height: "0px"}, 250);
-    } else {
-        $('.discover-path-img').stop().animate({height: "100vh"}, 1050);   
-    }
-});  
  
  $(window).scroll(function () {
   	var scroll = $(window).scrollTop();
 
-  	if (scroll >= 200) {
+  	if (scroll >= 300) {
   		$("header").addClass("nav-bg-change");
   	} else {
   		$("header").removeClass("nav-bg-change");
